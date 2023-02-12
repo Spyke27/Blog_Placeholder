da66ee4408a5bf0dae823a01db566a12e128b4f0
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-02-11 22:33:22 -0300

    Posts

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Post } from './types/Post'
+
+function App() {
+  const [posts, setPosts] = useState<Post[]>([])
+
+  return (
+    <div className="App">
+     
+     
+    </div>
+  )
+}
+
+export default App
